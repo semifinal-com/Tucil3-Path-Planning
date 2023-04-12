f141009ac48cd98c316b4067121b7652a498c566
rizkyrsyd28 <rizkyrasyid28@gmail.com>
2023-04-12 23:14:47 +0700

    bersihin file

--- src/Frontend/PathPlanning/src/app/app-routing.module.ts
+++ src/Frontend/PathPlanning/src/app/app-routing.module.ts
@@ -1,12 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './main/main.component';
-import { TestComponentComponent } from './test-component/test-component.component';
 
-const routes: Routes = [
-  { path: 'main', component: MainComponent },
-  { path: '', component: TestComponentComponent },
-];
+const routes: Routes = [{ path: '', component: MainComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/Frontend/PathPlanning/src/app/app.module.ts
+++ src/Frontend/PathPlanning/src/app/app.module.ts
@@ -4,11 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
-import { TestComponentComponent } from './test-component/test-component.component';
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [AppComponent, MainComponent, TestComponentComponent],
+  declarations: [AppComponent, MainComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
