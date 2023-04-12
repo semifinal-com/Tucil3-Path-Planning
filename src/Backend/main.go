e2dd7e9f7fefef28d0528edef76dee819a7761de
rizkyrsyd28 <rizkyrasyid28@gmail.com>
2023-04-12 17:23:05 +0700

    fix conflict

--- src/Backend/main.go
+++ src/Backend/main.go
@@ -1,58 +1,60 @@
-// package main
-
-// import C "Backend/Controller"
-// import "github.com/gin-gonic/gin"
-
-// func main() {
-// 	r := gin.Default()
-
-// 	userCtrl := C.UserController{}
-
-// 	userRoutes := r.Group("/users")
-// 	{
-// 		userRoutes.GET("", userCtrl.GetUsers)
-// 		userRoutes.POST("", userCtrl.CreateUser)
-// 		userRoutes.PUT("/:id", userCtrl.UpdateUser)
-// 		userRoutes.DELETE("/:id", userCtrl.DeleteUser)
-// 	}
-
-// 	r.Run()
-// }
-
 package main
 
-import C "Backend/Controller"
+import (
+	Control "Backend/Controller"
+	DataType "Backend/Model"
+	"github.com/gin-contrib/cors"
+)
 import "github.com/gin-gonic/gin"
 
 func main() {
-	jsonText := []byte(`{
-       "nodes": [
-           {
-               "id": 0,
-               "name": "A",
-               "coor": [0,0]
-           },
-           {
-               "id": 1,
-               "name": "B",
-               "coor": [2,2]
-           },
-           {
-               "id": 2,
-               "name": "C",
-               "coor": [4,0]
-           },
-           {
+	r := gin.Default()
 
-	userCtrl := C.UserController{}
+	r.Use(cors.Default())
 
-	userRoutes := r.Group("/users")
-	{
-		userRoutes.GET("", userCtrl.GetUsers)
-		userRoutes.POST("", userCtrl.CreateUser)
-		userRoutes.PUT("/:id", userCtrl.UpdateUser)
-		userRoutes.DELETE("/:id", userCtrl.DeleteUser)
-	}
+	var data DataType.Graph
+	var msg DataType.Message
+
+	r.POST("/", Control.PostHandler(&data, &msg))
+	r.GET("/", Control.GetHandler(&data, &msg))
 
 	r.Run()
 }
+
+//package main
+//
+//import C "Backend/Controller"
+//import "github.com/gin-gonic/gin"
+//
+//func main() {
+//	jsonText := []byte(`{
+//       "nodes": [
+//           {
+//               "id": 0,
+//               "name": "A",
+//               "coor": [0,0]
+//           },
+//           {
+//               "id": 1,
+//               "name": "B",
+//               "coor": [2,2]
+//           },
+//           {
+//               "id": 2,
+//               "name": "C",
+//               "coor": [4,0]
+//           },
+//           {
+//
+//	userCtrl := C.UserController{}
+//
+//	userRoutes := r.Group("/users")
+//	{
+//		userRoutes.GET("", userCtrl.GetUsers)
+//		userRoutes.POST("", userCtrl.CreateUser)
+//		userRoutes.PUT("/:id", userCtrl.UpdateUser)
+//		userRoutes.DELETE("/:id", userCtrl.DeleteUser)
+//	}
+//
+//	r.Run()
+//}
--- src/Frontend/PathPlanning/src/app/main/main.component.ts
+++ src/Frontend/PathPlanning/src/app/main/main.component.ts
@@ -12,7 +12,7 @@ import Point from 'ol/geom/Point';
 import LineString from 'ol/geom/LineString';
 import { Vector } from 'ol/source';
 import { Vector as VectorLayer } from 'ol/layer';
-import { Style, Stroke, Circle, Fill } from 'ol/style';
+import { Style, Stroke, Circle, Fill, Text } from 'ol/style';
 
 @Component({
   selector: 'app-main',
@@ -48,30 +48,35 @@ export class MainComponent implements OnInit {
         }),
       ],
       view: new View({
-        center: fromLonLat([107.616751, -6.890952]),
-        zoom: 15,
+        center: fromLonLat([2, 2]),
+        zoom: 10,
       }),
     });
   }
 
-  drawMarkersAndLine(): void {
+  draw2MarkersAndLine(coor1: any, name1: string, coor2: any, name2: string) {
     var marker1: Feature = new Feature({});
     var marker2: Feature = new Feature({});
     const markerSource = this.markerLayer.getSource();
 
+    console.log(coor1);
+
     // add first marker
     marker1 = new Feature({
-      geometry: new Point(fromLonLat([107.616817, -6.889568])),
+      geometry: new Point(fromLonLat([coor1[1], coor1[0]])),
+      nama: name1,
     });
     // add second marker
     marker2 = new Feature({
-      geometry: new Point(fromLonLat([107.618108, -6.89218])),
+      geometry: new Point(fromLonLat([coor2[1], coor2[0]])),
+      nama: name2,
     });
 
     // marker.setStyle(iconStyle);
     // this.markers.push(marker);
 
-    // this.vectorSource.addFeature(marker);
+    markerSource?.addFeature(marker1);
+    markerSource?.addFeature(marker2);
 
     // // If this is the second marker clicked, start adding a line between them
     // if (this.markers.length === 2 ) {
@@ -90,6 +95,7 @@ export class MainComponent implements OnInit {
           (<Point>marker1.getGeometry()).getCoordinates(),
           (<Point>marker2.getGeometry()).getCoordinates(),
         ]),
+        nama: 'dist',
       });
       lineSource.addFeature(line);
     }
@@ -105,7 +111,6 @@ export class MainComponent implements OnInit {
     reader.onload = () => {
       const fileContent = reader.result;
       this.MessageJSON = JSON.parse(fileContent as string);
-      console.log(this.MessageJSON);
     };
   }
 
@@ -114,18 +119,33 @@ export class MainComponent implements OnInit {
       features: [],
     });
 
-    this.markerLayer = new VectorLayer({
-      source: markerSource,
-      style: new Style({
-        image: new Circle({
-          radius: 7,
-          fill: new Fill({ color: 'red' }),
-          stroke: new Stroke({
-            color: 'white',
-            width: 2,
-          }),
+    const markerStyle = new Style({
+      image: new Circle({
+        radius: 7,
+        fill: new Fill({ color: 'red' }),
+        stroke: new Stroke({
+          color: 'white',
+          width: 2,
         }),
       }),
+      text: new Text({
+        text: '',
+        font: '12px sans-serif',
+        fill: new Fill({ color: 'black' }),
+        stroke: new Stroke({
+          color: 'white',
+          width: 2,
+        }),
+        offsetY: -15, // mengatur posisi teks ke atas
+      }),
+    });
+
+    this.markerLayer = new VectorLayer({
+      source: markerSource,
+      style: function (feature) {
+        markerStyle.getText().setText(feature.get('nama'));
+        return markerStyle;
+      },
     });
 
     this.map.addLayer(this.markerLayer);
@@ -136,22 +156,56 @@ export class MainComponent implements OnInit {
       features: [],
     });
 
-    this.lineLayer = new VectorLayer({
-      source: lineSource,
-      style: new Style({
+    const lineStyle = new Style({
+      stroke: new Stroke({
+        color: 'blue',
+        width: 5,
+      }),
+      text: new Text({
+        text: '',
+        font: '12px sans-serif',
+        fill: new Fill({ color: 'black' }),
         stroke: new Stroke({
-          color: 'blue',
-          width: 3,
+          color: 'white',
+          width: 2,
         }),
+        offsetY: -15, // mengatur posisi teks ke atas
       }),
     });
 
+    this.lineLayer = new VectorLayer({
+      source: lineSource,
+      style: function (feature) {
+        lineStyle.getText().setText(feature.get('nama'));
+        return lineStyle;
+      },
+    });
+
     this.map.addLayer(this.lineLayer);
+  }
+  createGraph(matrix: any, node: any) {
+    var MAXI: number = node.length;
+    var MAXJ: number = node[0].length;
+    for (let i: number = 0; i < 5; i++) {
+      for (let j: number = 0; j < 5; j++) {
+        console.log('test');
+        if (matrix[i][j] == 1) {
+          this.draw2MarkersAndLine(
+            node[i].coor,
+            node[i].name,
+            node[j].coor,
+            node[j].name
+          );
+        }
+      }
+    }
   }
 
   async onEnter(idStart: string, idDest: string) {
     var dest = parseInt(idDest);
     var strt = parseInt(idStart);
+
+    await this.createGraph(this.MessageJSON.mat, this.MessageJSON.nodes);
 
     this.MessageJSON.from = strt;
     this.MessageJSON.to = dest;
